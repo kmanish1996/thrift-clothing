28ad260b37e74c10c8864677b180a86fb450c107
kmanish1996 <kmanish1996@gmail.com>
2021-03-31 23:01:23 +0530

    Added Routing functionality using React Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component'
+import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
