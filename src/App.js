902e5de5d9903f06b2b611c9329a6c85f7a205bd
kmanish1996 <kmanish1996@gmail.com>
2021-04-04 18:27:39 +0530

    Added SignIn Form and Button components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { Switch, Route } from 'react-router-dom';
 
 
@@ -13,7 +14,8 @@ function App() {
     <Header/>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route   path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
